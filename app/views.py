bf26d91842ee92487251d3fe8063bef260a85143
Mateusz Wawro <101826182+MateuszWawro@users.noreply.github.com>
2022-09-12 18:22:17 +0200

    stowrzenie klasy formularzu

--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, url_for, request, redirect
 from app import app
+from .forms import NewPredicate
+
 
 
 ##widok strony głównej
 @app.route('/')
 def home_page():
     return render_template("index.html", title='')
-
-
-
+@app.route('/form')
+def form_page():
+    return render_template("formularz.html",title="")
