7ac25bd89d04bebd31110d89b650368118017e66
Mateusz Wawro <101826182+MateuszWawro@users.noreply.github.com>
2022-09-13 17:55:03 +0200

    dodanie walidacji pól formularza

--- app/forms.py
+++ app/forms.py
@@ -4,17 +4,17 @@ from wtforms.validators import DataRequired
 
 class NewPredicate (FlaskForm):
 
-    komorka = StringField('Komórka Organizacyjna')
-    kom_orz = StringField('Komórka Zgłaszająca')
-    nazwa_urz = StringField('Nazwa Urządzenia')
+    komorka = StringField('Komórka Organizacyjna',[validators.DataRequired()])
+    kom_orz = StringField('Komórka Zgłaszająca',[validators.DataRequired()])
+    nazwa_urz = StringField('Nazwa Urządzenia',[validators.DataRequired()])
     typ = StringField('Typ')
-    lata = IntegerField('Lata Urzydkowania')
-    cena = FloatField('Cena')
-    num_inw = StringField('Numer Inwentarzowy')
-    producent = StringField('Producent')
-    amortyzacja = StringField('Amortyzacja')
+    lata = IntegerField('Lata Urzydkowania',[validators.Length(min=1, max=2)])
+    cena = FloatField('Cena',[validators.DataRequired()])
+    num_inw = StringField('Numer Inwentarzowy',[validators.DataRequired()])
+    producent = StringField('Producent',[validators.DataRequired()])
+    amortyzacja = StringField('Amortyzacja',[validators.DataRequired()])
     num_fab = StringField('Numer Fabryczny')
-    opis = StringField('Opis Stanu Technicznego')
+    opis = StringField('Opis Stanu Technicznego',[validators.DataRequired()])
     generate = SubmitField('Generuj')
 
 
