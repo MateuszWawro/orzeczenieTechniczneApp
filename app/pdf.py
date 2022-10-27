16f9d7a252ffd4f87ade23c13324a974b3c81427
mwawro <mateusz.wawro4@gmail.com>
2022-10-27 14:08:08 +0200

    prac ciag dalszy xD

--- app/pdf.py
+++ app/pdf.py
@@ -11,11 +11,14 @@ def create_report(form=None):
    merge_bold = workbook.add_format({'align':'center','bold': True})
    merge_general = workbook.add_format({'align':'center'})
    merge_format = workbook.add_format({'align':'left','border': True})
+   merge_format.set_text_wrap()
+   merge_format.set_align('top')
    merge_general_left = workbook.add_format({'align':'left'})
 
 #stworzenie arkusza w pliku xlsx
    worksheet = workbook.add_worksheet()
    worksheet.set_landscape()
+
 
 #stworzenie prezentacji danych na arkuszu
 #lewa strona tytuł
@@ -54,25 +57,56 @@ def create_report(form=None):
    worksheet.merge_range('H9:I9', 'Nazwa Urzadzenia' ,merge_format)
    worksheet.merge_range('J9:M9', form.nazwa_urz.data ,merge_format)
 
+#lewa strona dane (2 zestaw tabel)
+   worksheet.merge_range('A11:F11', 'Charakterystyka urządzenia',merge_format)
 
+   worksheet.merge_range('A12:B12', 'a) typ', merge_format)
+   worksheet.merge_range('A13:B13', 'c) rok produkcji', merge_format)
+   worksheet.merge_range('A14:B14', 'e) czas ekslpoatacji', merge_format)
+   worksheet.merge_range('A15:B15', 'g) wartość księgowa', merge_format)
 
+   worksheet.merge_range('D12:E12', 'b) nr fab.', merge_format)
+   worksheet.merge_range('D13:E13', 'd) nr inw.', merge_format)
+   worksheet.merge_range('D14:E14', 'f) producent', merge_format)
+   worksheet.merge_range('D15:E15', 'h) amortyzacja', merge_format)
 
+#prawa strona dane (2 zestaw tabel)
+   worksheet.merge_range('H11:M11', 'Charakterystyka urządzenia',merge_format)
 
+   worksheet.merge_range('H12:I12', 'a) typ', merge_format)
+   worksheet.merge_range('H13:I13', 'c) rok produkcji', merge_format)
+   worksheet.merge_range('H14:I14', 'e) czas ekslpoatacji', merge_format)
+   worksheet.merge_range('H15:I15', 'g) wartość księgowa', merge_format)
 
+   worksheet.merge_range('K12:L12', 'b) nr fab.', merge_format)
+   worksheet.merge_range('K13:L13', 'd) nr inw.', merge_format)
+   worksheet.merge_range('K14:L14', 'f) producent', merge_format)
+   worksheet.merge_range('K15:L15', 'h) amortyzacja', merge_format)
+
+#opis lewa strona
+   worksheet.merge_range('A17:F17', 'Opis Stanu Technicznego', merge_format)
+
+   worksheet.merge_range('A18:F20', form.opis.data, merge_format)
+
+#opis prawa strona
+   worksheet.merge_range('H17:M17', 'Opis Stanu Technicznego', merge_format)
+
+   worksheet.merge_range('H18:M20', form.opis.data, merge_format)
 
 #koniec arkusza
 #lewa strona
    worksheet.merge_range('A26:B26', 'Zespół Orzekający:', merge_general_left)
    worksheet.merge_range('A29:B29', '1 .........................', merge_general_left)
    worksheet.merge_range('A33:B33', '2 .........................', merge_general_left)
-   worksheet.write('F26', 'Zatwierdzam')
+   worksheet.write('E26', 'Zatwierdzam')
 
 #prawa strona
    worksheet.merge_range('H26:I26', 'Zespół Orzekający:', merge_general_left)
    worksheet.merge_range('H29:I29', '1 .........................', merge_general_left)
    worksheet.merge_range('H33:I33', '2 .........................', merge_general_left)
-   worksheet.write('M26', 'Zatwierdzam')
+   worksheet.write('L26', 'Zatwierdzam')
 
+#koniec dokumentu
    workbook.close()
 
 
