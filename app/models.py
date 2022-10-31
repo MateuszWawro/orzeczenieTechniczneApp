f19166069a3025bb04251ae790e7d4c2123c52e5
mwawro <mateusz.wawro4@gmail.com>
2022-10-31 09:16:48 +0100

    dodanie nowego modelu bazy danych

--- app/models.py
+++ app/models.py
@@ -9,3 +9,20 @@ class User (db.Model, UserMixin):
     password = db.Column(db.String(32), nullable=False)
     name = db.Column(db.String(15))
     surname = db.Column(db.String(15))
+
+
+#model bazy danych orzeczeń
+class Orzeczenie(db.Model):
+    id =db.Column(db.Integer, primary_key=True)
+    kom_orz = db.Column(db.String())
+    komorka = db.Column(db.String())
+    nazwa_urz = db.Column(db.String())
+    typ = db.Column(db.String())
+    rok = db.Column(db.Integer)
+    lata = db.Column(db.String())
+    cena = db.Column(db.String())
+    num_inw = db.Column(db.String())
+    producent = db.Column(db.String())
+    amortyzacja = db.Column(db.String())
+    num_fab = db.Column(db.String())
+    opis = db.Column(db.String())
